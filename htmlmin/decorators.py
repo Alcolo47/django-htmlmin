36604c2a7ac1d25803988696483031befce2ab20
Alcolo47 <alcolo47@gmail.com>
2015-11-29 10:38:04 +0100

    Optimisation

--- htmlmin/decorators.py
+++ htmlmin/decorators.py
@@ -11,10 +11,9 @@ def minified_response(f):
     @wraps(f)
     def minify(*args, **kwargs):
         response = f(*args, **kwargs)
-        minifiable_status = response.status_code == 200
-        minifiable_content = 'text/html' in response['Content-Type']
-        if minifiable_status and minifiable_content:
+        if 'text/html' in response['Content-Type']:
             response.content = html_minify(response.content)
+            response.minify_response = False
         return response
 
     return minify
--- htmlmin/middleware.py
+++ htmlmin/middleware.py
@@ -17,30 +17,28 @@ class MarkRequestMiddleware(object):
 class HtmlMinifyMiddleware(object):
 
     def can_minify_response(self, request, response):
-        try:
-            req_ok = request._hit_htmlmin
-        except AttributeError:
+        if not getattr(request, '_hit_htmlmin', False):
             return False
 
         if hasattr(settings, 'EXCLUDE_FROM_MINIFYING'):
             for url_pattern in settings.EXCLUDE_FROM_MINIFYING:
                 regex = re.compile(url_pattern)
                 if regex.match(request.path.lstrip('/')):
-                    req_ok = False
-                    break
+                    return False
 
-        resp_ok = response.status_code == 200
-        resp_ok = resp_ok and 'text/html' in response['Content-Type']
-        if hasattr(response, 'minify_response'):
-            resp_ok = resp_ok and response.minify_response
-        return req_ok and resp_ok
+#         if response.status_code != 200:
+#             return False
+        if not 'text/html' in response['Content-Type']:
+            return False
+        return getattr(response, 'minify_response', True)
 
     def process_response(self, request, response):
         minify = getattr(settings, "HTML_MINIFY", not settings.DEBUG)
-        keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
-        parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
         if minify and self.can_minify_response(request, response):
+            keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
+            parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
             response.content = html_minify(response.content,
                                            ignore_comments=not keep_comments,
                                            parser=parser)
+            response.minify_response = False
         return response
